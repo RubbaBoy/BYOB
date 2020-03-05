95dbe83343cb4c176dbbdb67810578fc8e12901a
RubbaBoy <adamyarris@gmail.com>
2020-03-05 08:20:10 -0500

    Added writing of files, no pushing yet

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 void main(List<String> args) {
@@ -5,10 +6,11 @@ void main(List<String> args) {
   final label = args[1];
   final status = args[2];
   final color = args[3];
-  final typeString = args[4];
-  var path = args[5];
+  var path = args[4];
 
-  final type = Type.fromPath(path, typeString);
+  if (!path.startsWith('/')) {
+    path = '/$path';
+  }
 
   print('Make commit for badge:');
   print('name = $name');
@@ -16,46 +18,44 @@ void main(List<String> args) {
   print('status = $status');
   print('color = $color');
   print('\nLocation: $path');
-  print('File type: $type');
-
 
   print('Files:');
 
   final dir = Directory.current.absolute;
   print('Absolute: ${dir.path}');
 
-  final files = dir.listSync();
+  final shields = dir.listSync().firstWhere(
+      (entity) => entity.path.replaceFirst(dir.path, '') == path,
+      orElse: () =>
+          File('${dir.path}$path')..parent.createSync()..createSync()) as File;
 
-  print('Absolute all:\n${files.map((entity) => entity.path).join(', ')}');
-  print('Relative all:\n${files.map((entity) => entity.path.replaceFirst(dir.path, '')).join(', ')}');
+  print('Found file: ${shields.absolute.path}');
+
+  print(
+      'Absolute all:\n${dir.listSync().map((entity) => entity.absolute.path).join(', ')}');
+
+  final contents = jsonDecode(shields.readAsStringSync());
+
+  contents[name] = {
+    'label': label,
+    'status': status,
+    'color': color
+  };
+
+  final env = Platform.environment;
+  print(env);
+  print(env['GITHUB_ACTOR']);
+  print(env['REPOSITORY']);
+
+  Process.runSync('git', ['config', '--local', 'user.email', 'byob@yarr.is']);
+  Process.runSync('git', ['config', '--local', 'user.name', 'BYOB']);
+  Process.runSync('git', ['commit', '-m', 'Updating tag "$name"', '-a']);
+
+//  final remote = "https://${env['GITHUB_ACTOR']}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git";
+
+//  Process.runSync('git', ['push', '-m', 'Updating tag "$name"', '-a']);
+
+  shields.writeAsStringSync(jsonEncode(contents));
 
   print('Goodbye!');
-}
-
-class Type {
-  static const YAML = Type(['yaml', 'yml']);
-  static const JSON = Type(['json']);
-
-  static const List<Type> types = [YAML, JSON];
-
-  final List<String> extensions;
-
-  const Type(this.extensions);
-
-  /// Gets the [Type] from a value such as "yaml" or "json". If the [data] is
-  /// not either, it will to [fromString] to [orElse], which is [YAML] if
-  /// invalid again.
-  static Type fromString(String data, String orElse) {
-    data = data.toLowerCase();
-    return types.firstWhere((type) => type.extensions.contains(data), orElse: () => fromString(orElse, 'yaml'));
-  }
-
-  /// Gets the [Type] from the extension of a given file path. Invokes
-  /// [fromString] after a substring.
-  static Type fromPath(String data, String orElse) => fromString(data.substring(data.indexOf('.') + 1), orElse);
-
-  @override
-  String toString() {
-    return 'Type{extensions: $extensions}';
-  }
 }
