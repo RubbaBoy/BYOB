2881dce2c9236014a34ca164eb1d5a6f3c1ae043
RubbaBoy <adamyarris@gmail.com>
2020-03-05 22:20:30 -0500

    Cleaned up, fixed cloning issue

--- bin/main.dart
+++ bin/main.dart
@@ -20,9 +20,11 @@ void main(List<String> args) {
   final remote =
       'https://${env['GITHUB_ACTOR']}:$token@github.com/${env['GITHUB_REPOSITORY']}.git';
 
-  workingDir = Directory('${Directory.current.absolute.path}/repo-${DateTime.now().millisecondsSinceEpoch}');
+  final cloneInto = 'repo-${DateTime.now().millisecondsSinceEpoch}';
 
-  cloneRepo(branch, remote);
+  workingDir = Directory('${Directory.current.absolute.path}/$cloneInto');
+
+  cloneRepo(branch, remote, cloneInto);
 
   final shields = workingDir.listSync().firstWhere(
       (entity) => entity.path.replaceFirst(workingDir.path, '') == path,
@@ -44,26 +46,26 @@ void main(List<String> args) {
   runCommand('rm', ['-rf', workingDir.absolute.path], false);
 }
 
-void cloneRepo(String branch, String remote) {
-  print(runCommand('git', ['clone', remote, 'repo'], false));
+void cloneRepo(String branch, String remote, String cloneInto) {
+  runCommand('git', ['clone', remote, cloneInto], false);
 
-  final branchesCommand = runCommand('git', ['branch', '-a']);
-  final branches = branchesCommand.split('\n').where((line) => line.length >= 2).map((line) => line.substring(2).replaceFirst('remotes/origin/', '')).toList();
-  print('branches = $branches');
+  final branches = runCommand('git', ['branch', '-a'])
+      .split('\n')
+      .where((line) => line.length >= 2)
+      .map((line) => line.substring(2).replaceFirst('remotes/origin/', ''))
+      .toList();
   if (branches.contains(branch)) {
-    print('Branch "$branch" exists! Checking out...');
-    print(runCommand('git', ['checkout', branch]));
+    runCommand('git', ['checkout', branch]);
   } else {
-    print('Branch "$branch" not existant! Creating orphan...');
-    print(runCommand('git', ['checkout', '--orphan', branch]));
-    print(runCommand('git', ['rm', '-rf', '.']));
+    runCommand('git', ['checkout', '--orphan', branch]);
+    runCommand('git', ['rm', '-rf', '.']);
   }
 
   print('Done with repo stuff');
 }
 
-String runCommand(String cmd, [List<String> args = const [],
-  bool includeWorkingDir = true]) {
+String runCommand(String cmd,
+    [List<String> args = const [], bool includeWorkingDir = true]) {
   print('$cmd ${args.join(' ')}');
   final process = includeWorkingDir
       ? Process.runSync(cmd, args, workingDirectory: workingDir.absolute.path)
