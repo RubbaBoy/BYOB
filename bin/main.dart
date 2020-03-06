e40e22abc82fa8305c58b793dc0376b7ed3f0ad4
RubbaBoy <adamyarris@gmail.com>
2020-03-05 20:20:21 -0500

    Added branch option

--- bin/main.dart
+++ bin/main.dart
@@ -10,6 +10,7 @@ void main(List<String> args) {
   final color = args[3];
   var path = args[4];
   final token = args[5];
+  final branch = args[6];
 
   if (!path.startsWith('/')) {
     path = '/$path';
@@ -19,9 +20,9 @@ void main(List<String> args) {
   final remote =
       'https://${env['GITHUB_ACTOR']}:$token@github.com/${env['GITHUB_REPOSITORY']}.git';
 
-  runCommand('git', ['clone', remote, 'repo'], false);
-
   workingDir = Directory('${Directory.current.absolute.path}/repo');
+
+  cloneRepo(branch, remote);
 
   final shields = workingDir.listSync().firstWhere(
       (entity) => entity.path.replaceFirst(workingDir.path, '') == path,
@@ -37,17 +38,36 @@ void main(List<String> args) {
 
   runCommand('git', ['config', '--local', 'user.email', 'byob@yarr.is']);
   runCommand('git', ['config', '--local', 'user.name', 'BYOB']);
-  runCommand('git', ['add', '${shields.absolute.path}']);
+  runCommand('git', ['add', '.']);
   runCommand('git', ['commit', '-m', 'Updating tag "$name"']);
-  runCommand('git', ['push', remote, 'HEAD']);
+  runCommand('git', ['push', remote, branch]);
 }
 
-void runCommand(String cmd, List<String> args,
+void cloneRepo(String branch, String remote) {
+  print(runCommand('git', ['clone', remote, 'repo'], false));
+
+  final branches = runCommand('git', ['branch', '-a']);
+  final bran = branches.split('\n').map((line) => line.substring(2)).toList();
+  print('bran = $bran');
+  if (bran.contains(branch)) {
+    print('Branch "$branch" exists! Checking out...');
+    print(runCommand('git', ['checkout', branch]));
+  } else {
+    print('Branch "$branch" not existant! Creating orphan...');
+    print(runCommand('git', ['checkout', '--orphan', branch]));
+    print(runCommand('git', ['rm', '-rf', '.']));
+  }
+
+  print('Done with repo stuff');
+}
+
+String runCommand(String cmd, List<String> args,
     [bool includeWorkingDir = true]) {
   final process = includeWorkingDir
       ? Process.runSync(cmd, args, workingDirectory: workingDir.absolute.path)
       : Process.runSync(cmd, args);
   print(process.stderr);
+  return process.stdout;
 }
 
 dynamic safeDecode(String json) {
