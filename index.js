0dd58105f80c743df914692331c3fc6720db437e
RubbaBoy <adamyarris@gmail.com>
2020-09-13 16:04:47 -0400

    Transfer to Cloudflare Workers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const app = express();
-const request = require('request');
+/**
+ * Runnable on Cloudflare Workers
+ */
 
 const badgeNamePattern = /^[a-z_\-\d]{0,32}$/i;
 const namePattern = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
@@ -13,43 +13,43 @@ const error = 'FF0000';
 // RubbaBoy/EmojIDE/badgeName
 // RubbaBoy/EmojIDE/badgeName/master
 // RubbaBoy/EmojIDE/badgeName/master/path/here/stuff.json
-app.get(["/:nameorg/:repo/:name/:branch?", "/:nameorg/:repo/:name/:branch/*"], async (req, result) => {
-    let badgeName = req.params['name'];
-    let nameorg = req.params['nameorg'];
-    let repo = req.params['repo'];
-    let branch = req.params['branch'] || 'shields';
-    let path = req.params['0'] || 'shields.json';
+async function handleRequest(request) {
+    let url = new URL(request.url)
+    let splitted = url.pathname.substr(1).split('/')
+    let nameorg = splitted[0];
+    let repo = splitted[1];
+    let badgeName = splitted[2];
+    let branch = splitted[3] || 'shields';
+    let path = splitted[4] || 'shields.json';
 
     if (!badgeNamePattern.test(badgeName) || !namePattern.test(nameorg) || !repoPattern.test(repo) || !branchPattern.test(branch)) {
-        return sendResult(result, 'BYOB', 'Invalid request', error);
+        return sendResult('BYOB', 'Invalid request', error);
     }
 
-    console.log(`https://raw.githubusercontent.com/${nameorg}/${repo}/${branch}/${path}`);
-    request(`https://raw.githubusercontent.com/${nameorg}/${repo}/${branch}/${path}`, {}, (err, res, body) => {
+    let res = await fetch(`https://raw.githubusercontent.com/${nameorg}/${repo}/${branch}/${path}`)
 
-        if (res.statusCode < 200 || res.statusCode >= 300) {
-            return sendResult(result, 'BYOB', 'No config found (' + res.statusCode + ')', error);
-        }
+    if (res.status < 200 || res.status >= 300) {
+        return sendResult('BYOB', `No config found (${res.status})`, error);
+    }
 
-        let parsed = loadBody(body);
+    let parsed = loadBody(await res.text());
 
-        if (parsed === undefined) {
-            return sendResult(result, 'BYOB', 'Invalid file', error);
-        }
+    if (parsed === undefined) {
+        return sendResult('BYOB', 'Invalid file', error);
+    }
 
-        let currBadge = parsed[badgeName];
+    let currBadge = parsed[badgeName];
 
-        if (currBadge === undefined) {
-            return sendResult(result, 'BYOB', 'Badge not found', error);
-        }
+    if (currBadge === undefined) {
+        return sendResult('BYOB', 'Badge not found', error);
+    }
 
-        sendResult(result, currBadge['label'], currBadge['status'], currBadge['color']);
-    })
+    return sendResult(currBadge['label'], currBadge['status'], currBadge['color']);
+}
+
+addEventListener("fetch", event => {
+    return event.respondWith(handleRequest(event.request))
 })
-
-app.get('*', (req, res) => sendResult(res, 'BYOB', 'error', error))
-
-app.listen(3000, () => console.log(`BYOB listening at http://localhost:3000`))
 
 function loadBody(body) {
     try {
@@ -59,14 +59,14 @@ function loadBody(body) {
     }
 }
 
-function sendResult(result, label, status, color) {
-    return request(badgeUrl(label, status, color), {}, (e, r, b) => {
-        result.setHeader('ETag', Date.now());
-        result.setHeader('Cache-Control', 'no-cache');
-        result.setHeader('Pragma', 'no-cache');
-        result.setHeader('Content-Type', 'image/svg+xml');
-        result.send(b);
-    })
+async function sendResult(label, status, color) {
+    let res = await fetch(badgeUrl(label, status, color))
+    let response = new Response(res.body, res)
+    response.headers.append('ETag', Date.now())
+    response.headers.append('Cache-Control', 'no-cache')
+    response.headers.append('Pragma', 'no-cache')
+    response.headers.append('Content-Type', 'image/svg+xml')
+    return response
 }
 
 function badgeUrl(label, status, color) {
